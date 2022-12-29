f93dae7add766e8e40047bade22fa9a5ba597357
Albert Vasile <77061862+albertsk21@users.noreply.github.com>
2022-12-29 14:13:58 +0200

    Update infoDatabases.java

--- 2. Spring Data Databses Acces With JDBC/ex/Add Minion/src/infoDatabases.java
+++ 2. Spring Data Databses Acces With JDBC/ex/Add Minion/src/infoDatabases.java
@@ -1,5 +1,5 @@
 public class infoDatabases {
-    public static final String password = "Albert.nustiu.2002";
+    public static final String password = "1234";
     public static final String user = "root";
     public static final String url ="jdbc:mysql://localhost:3306/minions_db_access_to_jdbc";
 }
